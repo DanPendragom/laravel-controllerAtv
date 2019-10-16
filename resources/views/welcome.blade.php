82fecf6aa147c2bcf935c561115b4d024fccd589
DanPendragom <carlos.coutinho5@etec.sp.gov.br>
2019-10-16 14:18:08 +0000

    Estilização da página principal

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,100 +1,126 @@
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-
-        <title>Laravel</title>
-
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
-
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Nunito', sans-serif;
-                font-weight: 200;
-                height: 100vh;
-                margin: 0;
-            }
-
-            .full-height {
-                height: 100vh;
-            }
-
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
-
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 13px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
-    </head>
-    <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
-
-                        @if (Route::has('register'))
-                            <a href="{{ route('register') }}">Register</a>
-                        @endif
-                    @endauth
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Welcome</title>
+    <style>
+        *{
+            margin: 0;
+            padding: 0;
+            box-sizing: border-box;
+            font-family: Arial, Helvetica, sans-serif;
+        }
+        h1{
+            
+        }
+        .container{
+            display: grid;
+            grid-template-columns: 1fr 1fr;
+            grid-template-rows: 10vh 90vh auto;
+        }
+        .menu{
+            grid-column: 1/3;
+            grid-row: 1/2;
+            display: flex;
+            justify-content: space-between;
+            margin: 0 96px;
+        }
+        .menu img{
+            width: 200px;
+        }
+        .menu div{
+            background: #15614F;
+            height: 75px;
+            width: 50px;
+            border-radius: 0px 0px 15px 15px;
+        }
+        .section{
+            grid-column: 1/2;
+            grid-row: 2/3;
+            margin: 50px 96px;
+        }
+        .aside{
+            grid-column: 2/3;
+            grid-row: 2/3;
+            margin-top: 75px;
+            margin-right:40px;
+        }
+        .section.texts{
+            display: flex;
+            flex-direction: column;
+        }
+        .texts h1{
+            color: #15614F;
+            font-size: 50px;
+        }
+        .texts span{
+            color: #688B70;
+            font-size: 30px;
+        }
+        .section .options{
+            margin-top: 35px; 
+            display: grid;
+            grid-template-columns: 1fr 1fr;
+            grid-template-rows: auto auto auto;
+        }
+        .options button{
+            cursor: pointer;
+            outline: none;
+            box-shadow: -13px -10px 2px #D9F1DE;
+            margin: 25px 0;
+            color: white;
+            width: 205px;
+            height: 55px;
+            border-radius: 25px;
+            border: none;
+            font-weight: bolder;
+            font-size: 25px;
+            background-image: linear-gradient(to right, #15614F, #688B70);
+        }
+        .options button:hover{
+            transition: 0.1s;
+            box-shadow: 13px -10px 2px #97AF9C;
+        }
+        .options button:nth-child(1){
+            grid-column: 1/2;
+            grid-row: 1/2;
+        }
+        .options button:nth-child(2){
+            grid-column: 2/3;
+            grid-row: 2/3;
+        }
+        .options button:nth-child(3){
+            grid-column: 1/2;
+            grid-row: 3/4;
+        }
+        .aside img{
+            width: 100%;
+        } 
+    </style>
+</head>
+<body>
+    <div class="container">
+        <div class="menu">
+            <div>
+            </div>
+            <img src="{{ url('assets/laravelLogo.svg') }}" />
+        </div>
+        <div class="section">
+            <div class="texts">
+            <h1>W E L C O M E</h1>
+                <span>To the amazing PHP framework</span>
                 </div>
-            @endif
-
-            <div class="content">
-                <div class="title m-b-md">
-                    Laravel
-                </div>
-
-                <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://vapor.laravel.com">Vapor</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
-                </div>
+            <div class="options">
+                <button>MY IMC</button>
+                <button>GET CALC</button>
+                <button>GET SQUAD</button>
             </div>
         </div>
-    </body>
+        <div class="aside">
+            <img ondragstart='return false' src="{{ url('assets/img.svg') }}" />
+        </div>
+    </div>
+</body>
 </html>
